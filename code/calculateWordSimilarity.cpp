128ad813815558f54c9be2a9aa4bde35d9c453e5
Weikai Zhu <wiki_ki@163.com>
2015-05-05 16:26:54 +0800

    add calcws

--- code/calculateWordSimilarity.cpp
+++ code/calculateWordSimilarity.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -80,7 +81,20 @@ void    work() {
 
 double  calcws(string a , string b) {
     int a_index = index[a] , b_index = index[b];
-    return 0.0;
+	word wa = data[a_index];
+	word wb = data[b_index];
+	double sum = 0 , ab , abs_a = 0, abs_b = 0;
+	for (int i = 0;i < wa.linkWord.size();i ++)
+		for (int j = 0;j < wb.linkWord.size();j ++)
+			if (wa.linkWord[i] == wb.linkWord[j]) {
+				sum += wa.tfidf[i] * wb.tfidf[j];
+			}
+	for (int i = 0;i < wa.linkWord.size();i ++)
+		abs_a += wa.tfidf[i] * wa.tfidf[i];
+	for (int i = 0;i < wb.linkWord.size();i ++)
+		abs_b += wb.tfidf[i] * wb.tfidf[i];
+	ab = sqrt(abs_a * abs_b);
+    return sum/ab;
 }
 
 int main () {
